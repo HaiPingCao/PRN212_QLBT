13b0dc79c2fa89f25609baf40c098765872e5942
Haii26_lap_win <luongminhhai2882@gmail.com>
2026-06-30 18:54:15 +0700

    early built ui

--- QLBT/QLBT.Server/Views/AssignmentView.xaml.cs
+++ QLBT/QLBT.Server/Views/AssignmentView.xaml.cs
@@ -10,8 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using QLBT.Server;
 
-namespace QLBT.Server
+namespace QLBT.Server.Views
 {
     /// <summary>
     /// Interaction logic for AssignmentView.xaml
